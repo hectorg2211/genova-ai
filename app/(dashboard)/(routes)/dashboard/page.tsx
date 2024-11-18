f24c6df0f720bbf3c57c9538597987606b12b265
Hector Garcia <hector032716@gmail.com>
2024-11-18 08:58:42 -0800

    add base urls

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
-
 export default function Dashboard() {
   return (
     <div>
       <p>Dashboard Page (Protected)</p>
-
-      <UserButton />
     </div>
   )
 }
