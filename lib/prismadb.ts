567742c0d2a33162898aaf25ba7ef30a98c39f68
Hector Garcia <hector032716@gmail.com>
2024-11-21 19:40:47 -0800

    create prisma model

---
+++ lib/prismadb.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+const prismadb = globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.prisma = prismadb
+}
+
+export default prismadb
