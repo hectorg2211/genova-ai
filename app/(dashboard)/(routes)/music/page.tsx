5fe9764b907b57f1b22dec9619e21ffa85bd67e5
Hector Garcia <hector032716@gmail.com>
2024-11-21 18:53:03 -0800

    add music generation features

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -91,7 +91,11 @@ const MusicPage = () => {
 
           {!music && !isLoading && <Empty label='No music generated.' />}
 
-          <div>Music will be generated here</div>
+          {music && (
+            <audio controls className='w-full mt-8'>
+              <source src={music} />
+            </audio>
+          )}
         </div>
       </div>
     </div>
---
+++ app/api/music/route.ts
@@ -0,0 +1,57 @@
+import { auth } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import Replicate from 'replicate'
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY,
+})
+
+// Helper function to convert ReadableStream to Buffer
+async function streamToBuffer(stream: ReadableStream) {
+  const reader = stream.getReader()
+  const chunks = []
+  let done = false
+
+  while (!done) {
+    const { value, done: isDone } = await reader.read()
+    if (value) chunks.push(value)
+    done = isDone
+  }
+
+  return Buffer.concat(chunks)
+}
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = await auth()
+    const body = await req.json()
+    const { prompt } = body
+
+    if (!userId) return new NextResponse('Unauthorized', { status: 401 })
+    if (!prompt) return new NextResponse('Prompt is required', { status: 400 })
+    const response = (await replicate.run(
+      'riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05',
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    )) as { audio: ReadableStream }
+
+    if (response.audio) {
+      // Convert the ReadableStream to a buffer
+      const audioBuffer = await streamToBuffer(response.audio)
+
+      // Serve the audio as a Base64-encoded string or store it in a file storage service (e.g., AWS S3)
+      const base64Audio = audioBuffer.toString('base64')
+
+      // Return a data URI or process as needed
+      return NextResponse.json({ audio: `data:audio/mpeg;base64,${base64Audio}` })
+    }
+
+    return new NextResponse('Failed to generate audio', { status: 500 })
+  } catch (error) {
+    console.log('Music error', error)
+    return new NextResponse('Internal server error', { status: 500 })
+  }
+}
